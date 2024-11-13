451a71e1cfab3dc112f1752548d04ad90e2127be
Raphael Francipane <ruffif@gmail.com>
2024-11-13 23:27:51 +1100

    Level 5

--- Level4_pt_2.jl
+++ Level4_pt_2.jl
@@ -4,7 +4,7 @@ using Plots
 
 #define parameters for SIR model
 c = 8 #8 Estimated contacts per person
-β = 0.0352
+β = 0.0345
 
 
 #n = 10 #number of infection days
@@ -18,9 +18,6 @@ ps = 0.2 #20% chance of serious infection
 Φ = 0.8
 int_time = 30
 
-
-params = [c,β,γ]
-
 #pop starts at 6000, with one Infected
 S = 5999
 I = 1
@@ -29,7 +26,7 @@ R = 0
 
 #simulate 6 months
 tspan = [0,365/2]
-
+β_vals = [0.0329, 0.0345, 0.0357]
 #new data pre intervention
 
 data = [11,7,20,3,29,14,11,12,16,10,58,34,26,29,51,55,155,53,67,98,130,189,92,192,145,128,68,74,126,265,154,207,299,273,190,152,276,408,267,462,352,385]
@@ -37,27 +34,43 @@ data_time = [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,3
 data_s = [0,0,1,2,5,5,5,2,9,4,22,0,15,48,38,57,9,18,20,0,41,15,35,36,27,38,24,40,34,57,18,29,63,66,119,76]
 data_time_s = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56]
 
+plot_range(params, tspan, data, data_time, β_vals, 2, 6, ["SIRS Infected Population Prediction", "Time", "Infected Population", "DOH Infected Population Data"],
+            ["Worst case", "Mean case", "Best Case"])
+
+plot_range(params, tspan, data_s, data_time_s, β_vals, 3, 6, ["SIRS Seriously Infected Population Prediction", "Time", "Seriously Infected Population", "DOH Data"],
+            ["Worst case", "Mean case", "Best Case"])
+
+
+
 data_post_intervention = data[17:end]
 data_time_post_intervention = data_time[17:end]
 params = get_parameter_array(S, I, Is, R, c, β, γ, ps, γs, α, ϵ, Φ, int_time)
-plot_error(params, tspan, data_post_intervention, data_time_post_intervention, 2, 12, [0.4,0.9], ["Φ", "Φ vs Mean Squared Error"])
+plot_error(params, tspan, data_post_intervention, data_time_post_intervention, 2, 12, [0.4,0.9], ["Φ", "Φ vs Mean Squared Error (Time > 30 Days)"])
 Φ_range = get_parameter_range(params, tspan, data_post_intervention, data_time_post_intervention, [0.4,0.9],2, 12, 0.5)
 
+append!(Φ_range, 0)
 plot_range(params,tspan, data, data_time, Φ_range, 2, 12, ["SIRS Infected Population Prediction", "Time", "Infected Population", "DOH Infected Population Data"], 
-            ["Worst case", "Mean case", "Best Case"])
+            ["Worst case", "Mean case", "Best Case", "No Intervention"])
 
+plot_range(params,tspan, data_s, data_time_s, Φ_range, 3, 12, ["SIRS Seriously Infected Population Prediction", "Time", "Seriously Infected Population", "DOH Data"],
+            ["Worst case", "Mean case", "Best Case", "No Intervention"])
+
+
+
+data = [11,7,20,3,29,14,11,12,16,10,58,34,26,29,51,55,155,53,67,98,130,189,92,192,145,128,68,74,126,265,154,207,299,273,190,152,276,408,267,462,352,385,221,420,544,329,440,427,369,606,416,546,475,617,593,352,337,473,673,653,523,602,551,686,556,600]
+data_s = [0,0,1,2,5,5,5,2,9,4,22,0,15,48,38,57,9,18,20,0,41,15,35,36,27,38,24,40,34,57,18,29,63,66,119,76,95,28,109,136,119,104,121,93,147,129,130,161,133,136,138,139,181,181,218,183,167,164,219,220]
+data_time = [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]
+data_time_s = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]
+plot_range(params,tspan, data, data_time, Φ_range[1:3], 2, 12, ["SIRS Infected Population Prediction", "Time", "Infected Population", "DOH Infected Population Data"], 
+            ["Worst case", "Mean case", "Best Case"])
+plot_range(params,tspan, data_s, data_time_s, Φ_range[1:3], 3, 12, ["SIRS Seriously Infected Population Prediction", "Time", "Seriously Infected Population", "DOH Data"],
+            ["Worst case", "Mean case", "Best Case"])
+#=
 Φ = 0.6087 #Observed value
 
 solution = simulate_model(S, I, Is, R, c, β, γ, ps, γs, α, ϵ, Φ, int_time, tspan)
 plot_solution(solution, 2, data, data_time)
 plot_solution(solution, 3, data_s, data_time_s)
-
-#new data
-data = [11,7,20,3,29,14,11,12,16,10,58,34,26,29,51,55,155,53,67,98,130,189,92,192,145,128,68,74,126,265,154,207,299,273,190,152,276,408,267,462,352,385]
-data_time = [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56]
-data_s = [0,0,1,2,5,5,5,2,9,4,22,0,15,48,38,57,9,18,20,0,41,15,35,36,27,38,24,40,34,57,18,29,63,66,119,76]
-data_time_s = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56]
-
 
 plot_solution(solution, 2, data, data_time)
 plot_solution(solution, 3, data_s, data_time_s)
@@ -73,8 +86,4 @@ params = get_parameter_array(S, I, Is, R, c, β, γ, ps, γs, α, ϵ, Φ, int_ti
 plot_error(params, tspan, data_s, data_time_s, 3, 8, [0.,1])
 println(get_parameter_range(params, tspan, data_s, data_time_s, [0.15,0.3],3, 8, 0.2))
 
-ps = 0.1825
-solution = simulate_model(S, I, Is, R, c, β, γ, ps, γs, α, ϵ, Φ, int_time, tspan)
-plot_solution(solution, 2, data, data_time)
-plot_solution(solution, 3, data_s, data_time_s)
-plot_solution_SIRS(solution)
+=#
--- Level5.jl
+++ Level5.jl
@@ -10,12 +10,13 @@ c = 8 #8 Estimated contacts per person
 #n = 10 #number of infection days
 #p = 0.5 #chance of recovery in n days
 γ = 1/7 # 1/10
-ps = 0.1825 #20% chance of serious infection
+ps = 0.2 #20% chance of serious infection
 γs = 1/14 #serious infection for 14 days
 α = 1/30 # 1 month till Susceptible
 
 ϵ = 0.3
 Φ = 0.6067
+Φ_range = [0.476,0.609,0.768]
 int_time = 35
 
 
@@ -28,24 +29,52 @@ Is = 0
 R = 0
 
 #simulate 6 months
-tspan = [5,365/2]
+tspan = [9,365/2]
 
 #new data pre intervention
 
 
 #new data
-data = [21,29,25,30,28,34,28,54,57,92,73,80,109,102,128,135,163,150,211,196,233,247,283,286,332,371,390,404,467,529,598,641,704,702,788,856,854,955,995,1065,1106,1159,1217,1269,1298,1328,1339,1383,1431,1422,1414,1485,1464,1480]
-data_time = [27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]
-data_s = [3,3,4,7,3,8,7,5,9,13,15,3,20,13,11,20,16,11,15,18,27,24,28,36,41,35,41,55,63,66,72,80,90,104,109,115,127,135,147,162,163,186,194,200,216,223,241,249,258,275,277,299,302,300]
+data = [21, 29, 25, 30, 28, 34, 28, 54, 57, 92, 73, 80, 109, 102, 128, 135, 163, 150, 211, 196, 233, 247, 283, 286, 332, 371, 390, 404, 467, 529]
+data_time = [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56]
+data_s = [3, 3, 4, 7, 3, 8, 7, 5, 9, 13, 15, 3, 20, 13, 11, 20, 16, 11, 15, 18, 27, 24, 28, 36, 41, 35, 41, 55, 63, 66]
 data_time_s = data_time
 
-solution = simulate_model(S, I, Is, R, c, β, γ, ps, γs, α, ϵ, Φ, int_time, tspan)
 params = get_parameter_array(S, I, Is, R, c, β, γ, ps, γs, α, ϵ, Φ, int_time)
-variable_params = [6,7,8,9,10,12]
+variable_params = [6,7,8,9]
 
-plot_solution(solution,2,data,data_time, ["Infected population model", "Time", "Population Infected", "SIRS Model", "DOH Data"])
+#tspan[1] = find_start_time(params, 60, data, data_time, data_s, data_time_s, variable_params)
+tspan[1] = 9
+
+data = [21,29,25,30,28,34,28,54,57,92,73,80,109,102,128,135,163,150,211,196,233,247,283,286,332,371,390,404,467,529,598,641,704,702,788,856,854,955,995,1065,1106,1159,1217,1269,1298,1328,1339,1383,1431,1422,1414,1485,1464,1480]
+data_s = [3,3,4,7,3,8,7,5,9,13,15,3,20,13,11,20,16,11,15,18,27,24,28,36,41,35,41,55,63,66,72,80,90,104,109,115,127,135,147,162,163,186,194,200,216,223,241,249,258,275,277,299,302,300]
+data_time = [27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]
+data_time_s = data_time
+#params = optimise_parameters(params, tspan, data, data_time, data_s, data_time_s, variable_params, 20)
+
+#params = [9999.0, 1.0, 0.0, 0.0, 8.0, 0.038, 0.145, 0.134, 0.07, 0.03333333333333333, 0.3, 0.595, 35.0]
+params = [9999.0, 1.0, 0.0, 0.0, 8.0, 0.038, 0.145, 0.134, 0.07, 0.03333333333333333, 0.3, 0.609, 35.0]
+
+#Find β range
+plot_error(params, tspan, data, data_time, 2, 6, [0.02,0.04], ["β", "β vs Mean Squared Error"])
+β_range = get_parameter_range(params, tspan, data, data_time, [0.02,0.04],2, 6, 0.5)
+
+params[6] = β_range[2]
+
+plot_error(params, tspan, data_s, data_time_s, 3, 8, [0.05,0.3], ["ps", "ps vs Mean Squared Error"])
+ps_range = get_parameter_range(params, tspan, data_s, data_time_s, [0.05,0.3],3, 8, 0.5)
+params[8] = ps_range[2]
 
 
-plot_solution(solution,3,data_s,data_time_s, ["Seriously Infected population model", "Time", "Population Seriously Infected", "SIRS Model", "DOH Data"])
+plot_range(params,tspan, data, data_time, β_range, 2, 6, ["SIRS Infected Population Prediction", "Time", "Infected Population", "DOH Infected Population Data"], 
+            ["Worst case", "Mean case", "Best Case"])
 
-params = optimise_parameters(params, tspan, data, data_time, data_s, data_time_s, variable_params, 4)
+
+
+params = [9999.0,    1.0,    0.0,    0.0,    8.0,    0.035,    0.12,    0.163,    0.062,    0.03333333333333333,    0.3,    0.609,   35.0]
+plot_range(params,tspan, data, data_time, β_range, 2, 6, ["SIRS Infected Population Prediction", "Time", "Infected Population", "DOH Infected Population Data"], 
+            ["Worst case", "Mean case", "Best Case"])
+plot_range(params,tspan, data, data_time, Φ_range, 2, 12, ["SIRS Infected Population Prediction", "Time", "Infected Population", "DOH Infected Population Data"], 
+            ["Worst case", "Mean case", "Best Case"])
+plot_range(params,tspan, data_s, data_time_s, Φ_range, 3, 12, ["SIRS Seriously Infected Population Prediction", "Time", "Seriously Infected Population", "DOH Data"],
+            ["Worst case", "Mean case", "Best Case"])
